3bc55e41f35ba6e71eb0efd2c5e3bb7926b5e1e6
Roshan Basu <roshanbasu7@gmail.com>
2024-12-24 18:55:41 +0530

    Adding utils for specs and committing test cases till a failure occured

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,13 +1,94 @@
 require 'rspec'
 require_relative '../calculator/calculator'
+require_relative "./spec_utils.rb"
 
 RSpec.describe Calculator do
 
+  include SpecUtils
   describe 'add numbers successfully' do
     context "when adding numbers which are comma seperated" do
-      it 'adds all numbers' do
+      it 'adds all numbers which have only comma between them' do
         calculator = Calculator.new
-        expect(calculator.add("3,5")).to eq(8)
+        data_generator = SpecUtils::NumberAndSumGenerator.new(2)
+        data_generator.generate_test_data
+        expect(calculator.add("#{data_generator.numbers[0]},#{data_generator.numbers[1]}")).to eq(data_generator.final_sum)
+      end
+
+      it 'adds any number of numbers which have only comma between them' do
+        calculator = Calculator.new
+        data_generator = SpecUtils::NumberAndSumGenerator.new(rand(10))
+        data_generator.generate_test_data
+        expect(calculator.add("#{data_generator.numbers.join(',')}")).to eq(data_generator.final_sum)
+      end
+
+      it 'adds numbers which have comma and spaces between them' do
+        calculator = Calculator.new
+        data_generator = SpecUtils::NumberAndSumGenerator.new(2)
+        data_generator.generate_test_data
+        spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100), " ")
+        expect(calculator.add("#{data_generator.numbers[0]}," + spaces +  "#{data_generator.numbers[1]}")).to eq(data_generator.final_sum)
+      end
+
+      it 'should give 0 if no number in input, with or without any whitelines' do
+        calculator = Calculator.new
+        spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100))
+        expect(calculator.add(spaces)).to  eq(0)
+        expect(calculator.add("")).to  eq(0)
+      end
+
+      it 'should give same number as input if only one number in input, without whitespaces in prefix and suffix' do
+        calculator = Calculator.new
+        number = rand(100)
+        expect(calculator.add(number.to_s)).to  eq(number)
+      end
+
+      it 'should give same number as input if only one number in input, with whitespaces in prefix, not in suffix' do
+        calculator = Calculator.new
+        number = rand(100)
+        prefix_spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100))
+        expect(calculator.add("#{prefix_spaces}#{number}")).to  eq(number)
+      end
+
+      it 'should give same number as input if only one number in input, with whitespaces not in prefix, but present in suffix' do
+        calculator = Calculator.new
+        number = rand(100)
+        suffix_spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100))
+        expect(calculator.add("#{number}#{suffix_spaces}")).to  eq(number)
+      end
+
+      it 'should give same number as input if only one number in input, with prefix and suffix spaces' do
+        calculator = Calculator.new
+        number = rand(100)
+        prefix_spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100))
+        suffix_spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100))
+        expect(calculator.add("#{prefix_spaces}#{number}#{suffix_spaces}")).to  eq(number)
+      end
+
+      it 'should add numbers even if newlines are present between commas' do
+        calculator = Calculator.new
+        data_generator = SpecUtils::NumberAndSumGenerator.new(rand(100))
+        data_generator.generate_test_data
+        input_string = ""
+        data_generator.numbers.each_with_index do |number, index|
+          input_string = input_string + number.to_s + SpecUtils::WhiteSpaceStringGenerator.generate(rand(10), "\n")
+          input_string = input_string + "," if index < data_generator.numbers.length - 1
+        end
+        expect(calculator.add(input_string)).to  eq(data_generator.final_sum)
+      end
+    end
+
+    context "when adding number which have a different delimiter" do
+      def random_non_alphanumeric_character
+        non_alphanumeric_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/']
+        non_alphanumeric_characters[rand(non_alphanumeric_characters.length)]
+      end
+      let(:delimiter) { random_non_alphanumeric_character }
+
+      it 'adds all numbers which have only custom delimiter between them' do
+        calculator = Calculator.new
+        data_generator = SpecUtils::NumberAndSumGenerator.new(2)
+        data_generator.generate_test_data
+        expect(calculator.add("//#{delimiter}\n#{data_generator.numbers[0]}#{delimiter}#{data_generator.numbers[1]}")).to eq(data_generator.final_sum)
       end
     end
   end
---
+++ spec/spec_utils.rb
@@ -0,0 +1,46 @@
+module SpecUtils
+
+
+  class NumberAndSumGenerator
+    attr_reader :no_of_numbers, :final_sum, :numbers
+    def initialize(no_of_numbers)
+      @no_of_numbers = no_of_numbers
+      @final_sum = 0
+      @numbers = []
+    end
+
+    def generate_test_data
+      @final_sum = 0
+      generate_numbers
+      calculate_sum
+    end
+
+    private
+
+    def generate_numbers
+      no_of_numbers.times {
+        # rand is non-negative by default, in case code is reviewed
+        @numbers << rand(1..100)
+      }
+    end
+
+    def calculate_sum
+      @final_sum = numbers.sum
+    end
+
+  end
+
+  class WhiteSpaceStringGenerator
+    WHITESPACE_CHARACTERS = [" ", "\t", "\n", "\r", "\f", "\v"]
+    def self.generate(no_of_times, whitespace=nil)
+      final_string = ""
+      no_of_times.times {
+        if whitespace.nil?
+          whitespace = WHITESPACE_CHARACTERS[rand(WHITESPACE_CHARACTERS.length)]
+        end
+        final_string = final_string + whitespace
+      }
+      final_string
+    end
+  end
+end
