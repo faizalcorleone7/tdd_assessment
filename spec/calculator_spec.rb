f10a10b3a2005d07c8e20716aee76e4fa4d7e589
Roshan Basu <roshanbasu7@gmail.com>
2024-12-24 20:00:03 +0530

    Adding test for custom delimiter with length greater than 1

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -78,17 +78,28 @@ RSpec.describe Calculator do
     end
 
     context "when adding number which have a different delimiter" do
-      def random_non_alphanumeric_character
+      def random_non_alphanumeric_character(delimiter_length=1)
         non_alphanumeric_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/']
-        non_alphanumeric_characters[rand(non_alphanumeric_characters.length)]
+        delimiter_string = ""
+        delimiter_length.times { delimiter_string << non_alphanumeric_characters[rand(non_alphanumeric_characters.length)] }
+        delimiter_string
       end
       let(:delimiter) { random_non_alphanumeric_character }
+      let(:multiple_length_delimtier) { random_non_alphanumeric_character(rand(100)) }
 
       it 'adds all numbers which have only custom delimiter between them' do
         calculator = Calculator.new
         data_generator = SpecUtils::NumberAndSumGenerator.new(2)
         data_generator.generate_test_data
         expect(calculator.add("//#{delimiter}\n#{data_generator.numbers[0]}#{delimiter}#{data_generator.numbers[1]}")).to eq(data_generator.final_sum)
+      end
+
+      it 'adds numbers which have custom delimiter with multiple character length' do
+        calculator = Calculator.new
+        data_generator = SpecUtils::NumberAndSumGenerator.new(2)
+        data_generator.generate_test_data
+        spaces = SpecUtils::WhiteSpaceStringGenerator.generate(rand(100), " ")
+        expect(calculator.add("//#{multiple_length_delimtier}\n#{data_generator.numbers[0]}#{multiple_length_delimtier}" + spaces +  "#{data_generator.numbers[1]}")).to eq(data_generator.final_sum)
       end
 
       it 'adds any number of numbers which have only custom delimiter between them' do
