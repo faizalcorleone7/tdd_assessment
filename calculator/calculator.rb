2d3a6f60e15b86bbf4d3167eb7a888879d9968b2
Roshan Basu <roshanbasu7@gmail.com>
2024-12-24 19:13:05 +0530

    Refactoring class with delimiter parsing logic

--- calculator/calculator.rb
+++ calculator/calculator.rb
@@ -1,5 +1,19 @@
 class Calculator
+
+  attr_reader :delimiter
   def add(numbers_string)
-    numbers_string.split(",").map(&:to_i).sum
+    parse(numbers_string)
+    numbers_string.split(delimiter).map(&:to_i).sum
+  end
+
+  private
+
+  def parse(numbers_string)
+    parsed_delimiter = nil
+    if numbers_string.include?("//")
+      segments = numbers_string.split("//")
+      parsed_delimiter = segments[1].split("\n").first
+    end
+    @delimiter = parsed_delimiter || ","
   end
 end
